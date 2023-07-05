59fb68c1f1b488388d95861278991d7812fff2fc
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-07-05 21:34:39 +0100

    added style to loading screen

--- Notes/client/src/App.jsx
+++ Notes/client/src/App.jsx
@@ -11,24 +11,24 @@ function App() {
   const [notes, setNotes] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [noteToEdit, setNoteToEdit] = useState({});
-  const [loginName, setLoginName] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [loginName, setLoginName] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const email = cookies.Email;
   const authToken = cookies.AuthToken;
 
   const getData = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const response = await fetch(
         `${import.meta.env.VITE_APP_SERVERURL}/notes/${email}`
       );
       const json = await response.json();
-      setLoading(false)
       setNotes(json);
+      setLoading(false);
       // console.log(notes);
     } catch (err) {
-      setLoading(false)
+      setLoading(false);
       console.error(err);
     }
   };
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <div className="app">
-      {!authToken && <Auth setLoginName={setLoginName}/>}
+      {!authToken && <Auth setLoginName={setLoginName} />}
       {authToken && (
         <>
           <Navigation loginName={loginName} />
@@ -52,7 +52,17 @@ function App() {
             noteToEdit={noteToEdit}
             setIsEditing={setIsEditing}
           />
-          {loading && <h2>Loading...</h2>}
+          {loading && (
+            <h2
+              style={{
+                textAlign: "center",
+                color: "var(--heading-color)",
+                margin: "1rem",
+              }}
+            >
+              Loading...
+            </h2>
+          )}
           <section id="note-section">
             {notes?.map((note) => (
               <Note
